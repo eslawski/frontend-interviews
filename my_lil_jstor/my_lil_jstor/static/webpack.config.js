e566338126f3688717de17a10c2d87cbd41b5643
Robert Gaul <gaulrobe@gmail.com>
2017-10-04 10:40:23 -0400

    HOTH-1336: Get some static assets set up with bonsai

---
+++ my_lil_jstor/my_lil_jstor/static/webpack.config.js
@@ -0,0 +1,46 @@
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin("[name]");
+
+module.exports = {
+    entry: {
+        "dist/css/my-lil-jstor.css": "./scss/my-lil-jstor.scss"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: "sass-loader",
+                        options: {
+                            includePaths: [
+                                "scss",
+                                "node_modules",
+                                "node_modules/@ithaka"
+                            ],
+                            sourceMap: true,
+                            outputStyle: "expanded"
+                        }
+                    }],
+                    fallback: "style-loader"
+                })
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+                loader: "url-loader?name=./dist/fonts/[hash].[ext]"
+            }
+        ]
+    },
+    output: {
+        filename: "[name]"
+    },
+    plugins: [
+        extractSass
+    ]
+};
